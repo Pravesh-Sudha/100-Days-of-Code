2493143632cf1e084af571040d86b83603e0fb26
Pravesh-Sudha <programmerpravesh@gmail.com>
2023-08-28 11:58:07 +0530

    Main.go of weather app fixed

--- day-10/weather-app/main.go
+++ day-10/weather-app/main.go
@@ -44,7 +44,7 @@ func query(city string) (weatherData, error){
 	if err != nil{
 		return weatherData{}, err
 	}
-	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID="+ apiConfig.OpenWeatherApiKey + "&q=" + city)
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID="+ apiConfig.OpenWeatherMapApiKey + "&q=" + city)
 	
 	if err != nil {
 		return weatherData{}, err
